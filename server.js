2ade5430a6971ebf3aa7ad92212551cad20fbabd
DerangulaLokesh333 <derangulalokesh200@gmail.com>
2026-09-18 15:56:38 +0530

    Handle product POST request with Axios

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const path = require('path');
 
 const app = express();
 
+
+app.use(express.json());
+
+
 app.get('/api/products', (req, res) => {
     res.sendFile(path.join(__dirname, 'VIEW', 'index.html'));
+});
+
+
+app.post('/api/products', (req, res) => {
+    const product = req.body;
+
+    console.log('Product received:', product);
+
+    res.json(product);
 });
 
 const PORT = 4000;
